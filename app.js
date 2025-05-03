a47c5d86f8bd2c6fd9a51bdca3bf1ddcc9eb3e5c
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2025-05-04 01:54:11 +0700

    feat:test frontend with ejs

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require("express");
-const cors = require("cors"); // Tambahkan ini
+const cors = require("cors");
+const expressLayout = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 5000;
 
-// Izinkan permintaan dari semua origin (atau bisa dibatasi ke origin tertentu)
 app.use(cors()); // Middleware CORS
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Templating layout
+app.get("/", (req, res) => {
+  res.render("index"); // akan otomatis cari di views/index.ejs
+});
+app.set("view engine", "ejs");
 
 // Endpoint API
 app.get("/api/data", (req, res) => {
   res.json({ message: "Halo dari Backend Node.js!" });
 });
 
-// Jalankan server di semua IP agar bisa diakses dari jaringan lokal
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server backend berjalan di http://0.0.0.0:${port}`);
 });
